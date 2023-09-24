cded0be5c5ae3fa1aea46dbd856c023104e9a9dd
AlexeyManankov <virtbrain@gmail.com>
2023-09-24 12:40:31 +0500

    Initial Commit

---
+++ KeychainSwiftUI/KeychainSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeychainSwiftUIApp.swift
+//  KeychainSwiftUI
+//
+//  Created by Alexey Manankov on 24.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct KeychainSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
